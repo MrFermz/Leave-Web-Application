dd7fb9ead4bc6c597fb0db8969b3ac3fe987627d
MrFermz <Mr.Fermz@hotmail.com>
2020-02-11 15:08:29 +0700

    add filter content type

--- api/server.js
+++ api/server.js
@@ -4,25 +4,30 @@ const config        =       require('./config.json')
 const api           =       require('./api')
 
 const app           =   http.createServer(function (req, res) {
-    let body    =   []
+    let body        =   []
+    let contentType =   req.headers['content-type']
 
     // set headers
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
     res.setHeader('Access-Control-Allow-Headers', '*')
-    res.setHeader('Content-Type', `${req.headers["content-type"]}`)
+    res.setHeader('Content-Type', `${contentType}`)
 
     // get data
     req.on('data', chunk => {
         body.push(chunk)
     }).on('end', () => {
-        body = Buffer.concat(body).toString()
-        res.writeHead(200, { 'Content-Type': `${req.headers["content-type"]}` })
+        if (contentType == 'image/jpeg') {
+            body = Buffer.concat(body)
+        } else {
+            body = Buffer.concat(body).toString()
+        }
+        res.writeHead(200, { 'Content-Type': `${contentType}` })
         api.callAPI(req, res, body)
     })
 })
 
 
-app.listen(config.port, () => {
-    console.log(`Running... ${config.port}`)
+app.listen(config.sql.port, () => {
+    console.log(`Running... ${config.sql.port}`)
 })
