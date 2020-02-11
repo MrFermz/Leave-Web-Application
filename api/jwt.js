1fbd99724f7256539e0f230d7d2ea0e8dcf459f4
MrFermz <Mr.Fermz@hotmail.com>
2020-02-11 11:44:57 +0700

    refactor

--- api/jwt.js
+++ api/jwt.js
@@ -1,21 +1,22 @@
 const jwt               =       require('jsonwebtoken')
 const secreteKey        =       'MrFermz'
 const result            =       {
-
                                     auth        :   false,
                                     message     :   ''
-
                                 }
 
 function getToken(json) {
-    return token        =       jwt.sign(json, secreteKey, {
-        expiresIn: 8640000 // expires in 24 hours
-    })
+    return token        =       jwt.sign(
+                                    json, 
+                                    secreteKey, {
+                                        expiresIn: 8640000 // expires in 24 hours
+                                    }
+                                )
 }
 
 
 function verifyToken(req, res) {
-    let token       =   req.headers['x-access-token']
+    let token       =       req.headers['x-access-token']
     return new Promise(function (resolve, reject) {
         if (token) {
             jwt.verify(token, secreteKey, function (error, decoded) {
