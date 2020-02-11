b8b68a5e1cc58c0d94d42a58cbed5f3d079f3922
MrFermz <Mr.Fermz@hotmail.com>
2020-02-11 11:44:34 +0700

    change headers to dynamic value

--- api/server.js
+++ api/server.js
@@ -10,14 +10,14 @@ const app           =   http.createServer(function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
     res.setHeader('Access-Control-Allow-Headers', '*')
-    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Content-Type', `${req.headers["content-type"]}`)
 
     // get data
     req.on('data', chunk => {
         body.push(chunk)
     }).on('end', () => {
         body = Buffer.concat(body).toString()
-        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.writeHead(200, { 'Content-Type': `${req.headers["content-type"]}` })
         api.callAPI(req, res, body)
     })
 })
