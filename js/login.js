7621eb6074df9701c2fc95624c1ea8600c2a15df
MrFermz <Mr.Fermz@hotmail.com>
2020-02-12 11:55:43 +0700

    remove genContent func

--- js/login.js
+++ js/login.js
@@ -1,31 +1,33 @@
-const http = new XMLHttpRequest()
-var username, password
+const http              = new XMLHttpRequest()
+var TOKEN               = getToken()
+var USERNAME
+var PASSWORD
 
 
 function onLoad() {
-    genContent()
+    let load    = templateLogin()
+    document.getElementById('container').innerHTML  =   load
 }
 
 
 function onChange() {
-    username        =       document.getElementById('username').value
-    password        =       document.getElementById('password').value
+    USERNAME    = document.getElementById('username').value
+    PASSWORD    = document.getElementById('password').value
 }
 
 
 function onLogin() {
-    const data  =   {
-                        username        :       username,
-                        password        :       password
-                    }
-
-    if (username, password) {
+    const data      = {
+                        username    : USERNAME,
+                        password    : PASSWORD
+                      }
+    if (USERNAME, PASSWORD) {
         http.open('POST', 'http://localhost:8081/login', true)
         http.setRequestHeader('Content-Type', 'application/json')
         http.send(JSON.stringify(data))
         http.onreadystatechange = function () {
             if (this.readyState === 4 && this.status === 200) {
-                let res     =       JSON.parse(this.responseText)
+                let res     = JSON.parse(this.responseText)
                 localStorage.setItem('type', res['type'])
                 loginCheck(res['result'], res['data'])
             }
@@ -37,44 +39,7 @@ function onLogin() {
 function loginCheck(result, token) {
     if (result === 'success') {
         localStorage.setItem('token', token)
-        localStorage.setItem('username', username)
+        localStorage.setItem('username', USERNAME)
         changePage('home')
     }
-}
-
-
-function genContent() {
-    let header          =           document.createElement('div')
-    let username        =           document.createElement('input')
-    let password        =           document.createElement('input')
-    let submit          =           document.createElement('input')
-    let token      =           document.createElement('input')
-
-    header.innerHTML    =           'LOGIN'
-
-    username.setAttribute('id', 'username')
-    username.setAttribute('type', 'text')
-    username.setAttribute('placeholder', 'Username')
-    username.onchange   =           () => onChange()
-
-    password.setAttribute('id', 'password')
-    password.setAttribute('type', 'password')
-    password.setAttribute('placeholder', 'Password')
-    password.onchange   =           () => onChange()
-
-    submit.setAttribute('id', 'submit')
-    submit.setAttribute('type', 'submit')
-    submit.setAttribute('value', 'Login')
-    submit.onclick      =           () => onLogin()
-
-    token.setAttribute('id', 'token')
-    token.setAttribute('type', 'button')
-    token.setAttribute('value', 'Token')
-    token.onclick      =           () => checkToken()
-
-    document.getElementById('container').appendChild(header)
-    document.getElementById('container').appendChild(username)
-    document.getElementById('container').appendChild(password)
-    document.getElementById('container').appendChild(submit)
-    document.getElementById('container').appendChild(token)
 }
