2f6a7649e097512ceca103bd171f9a1f8567c550
MrFermz <Mr.Fermz@hotmail.com>
2020-02-14 15:07:14 +0700

    get image path from mongo

--- api/method/get/lists_appr_leaves.js
+++ api/method/get/lists_appr_leaves.js
@@ -1,4 +1,5 @@
 const db                                    = require('../../db_connection')
+const mongo                                 = require('../../mg_connection')
 const { verifyToken }                       = require('../../jwt')
 const { result_success, result_failed }     = require('../result')
 
@@ -6,16 +7,44 @@ const { result_success, result_failed }     = require('../result')
 async function listsApprLeaves(req, res) {
     let token       = await verifyToken(req, res)
     if (token) {
-        let sql     = `SELECT *
+        let sql     = `SELECT leaves.leaveID,
+                              leaves.leaveType,
+                              leaves.dateStart,
+                              leaves.dateEnd,
+                              leaves.reasons,
+                              leaves.status,
+                              leaves.UID,
+                              leaves.uploadID
                        FROM leaves
-                       WHERE status = 0`
-        db.query(sql, function (error, result) {
+                       WHERE leaves.status = 0`
+        db.query(sql, async function (error, result) {
+            let leaves              = result
+            let images              = []
+            let final_result        = []
             if (error) {
                 result_failed['data']   = error
                 res.end(JSON.stringify(result_failed))
             } else {
-                result_success['data']  = result
-                res.end(JSON.stringify(result_success))
+                let mongodb     = await mongo()
+                mongodb.collection('uploads').find({}).toArray((error, image) => {
+                    images          = image
+                    for (let leave of leaves) {
+                        let uploadID    = leave.uploadID
+                        for (const image of images) {
+                            let imageID     = image.id
+                            if (uploadID == imageID) {
+                                leave.path =  image.path
+                            }
+                        }
+                    }
+                    if (error) {
+                        result_failed['data']   = error
+                        res.end(JSON.stringify(result_failed))
+                    } else {
+                        result_success['data']  = result
+                        res.end(JSON.stringify(result_success))
+                    }
+                })
             }
         })
     } else {
