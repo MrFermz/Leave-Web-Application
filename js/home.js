ecd2e9d17401e72285b0db7dd0b79b132195952b
MrFermz <Mr.Fermz@hotmail.com>
2020-02-15 14:34:59 +0700

    add leavemax params

--- js/home.js
+++ js/home.js
@@ -9,10 +9,11 @@ function onLoad() {
 
 async function genContent() {
     let leaveDays       = await sqlQueriesGET('listsleavedays')
+    let leaveMax        = await sqlQueriesGET('listsleavemax')
     let pendings        = await sqlQueriesGET('listspendings')
     let sidebar         = await templateSidebar()
     let header          = await templateHeader()
-    let cardLeave       = await templateCardHome(leaveDays[0], 'leave')
+    let cardLeave       = await templateCardHome(leaveDays[0], 'leave', leaveMax)
     let cardPending     = await templateCardHome(pendings[0], 'pending')
     let markup          = sidebar + header + cardLeave + cardPending
     document.getElementById('container').innerHTML  =   markup
