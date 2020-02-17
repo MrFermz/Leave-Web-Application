f4c385d7f262d62c700f58cea2e726fcd28e4963
MrFermz <Mr.Fermz@hotmail.com>
2020-02-17 17:03:57 +0700

    add SQL query

--- assets/queries/sql_query.js
+++ assets/queries/sql_query.js
@@ -16,6 +16,22 @@ function sqlQueriesGET(path) {
 }
 
 
+function sqlQueriesLEAVEDAYS(path, id) {
+    http.open('GET', `http://localhost:8081/${path}`, true)
+    http.setRequestHeader('x-access-token', TOKEN)
+    http.setRequestHeader('leaveDaysID', id)
+    http.send()
+    return new Promise(function (resolve, reject) {
+        http.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                let data    = JSON.parse(this.responseText)
+                resolve(data.data)
+            }
+        }
+    })
+}
+
+
 function sqlQueriesPOST(path, data) {
     http.open('POST', `http://localhost:8081/${path}`, true)
     http.setRequestHeader('x-access-token', TOKEN)
