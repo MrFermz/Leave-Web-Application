20488d1a2dce47d698e8e6e612109bfa8d07ccee
MrFermz <Mr.Fermz@hotmail.com>
2020-02-17 17:13:41 +0700

    add +1 leavedays

--- api/method/post/create_leaves.js
+++ api/method/post/create_leaves.js
@@ -1,4 +1,5 @@
 const db                                    = require('../../db_connection')
+const mongo                                 = require('../../mg_connection')
 const { verifyToken }                       = require('../../jwt')
 const { result_success, result_failed }     = require('../result')
 
@@ -22,30 +23,92 @@ async function createleaves(req, res, body) {
                             data.status,
                             token.id
                           ]]
-        db.query(sql, [values], function (error, result) {
+        db.query(sql, [values], async function (error, result) {
             let insertId        = result.insertId
-
+            
             if (error) {
                 result_failed['data']   =   error
                 res.end(JSON.stringify(result_failed))
             } else {
-                let sql     = `UPDATE   leaves
-                               SET      uploadID    = ${insertId}
-                               WHERE    leaveID     = ${insertId}`
-                db.query(sql, function (error, result) {
-                    if (error) {
-                        result_failed['data']       = error
-                        res.end(JSON.stringify(result_failed))
-                    } else {
-                        result_success['data']      = insertId
+                let leavedays       = await usedleavedays(token.id, data.leaveType)
+                if (leavedays.result == 'success') {
+                    let updateleave         = await updateleaves(insertId)
+                    if (updateleave.result == 'success') {
                         res.end(JSON.stringify(result_success))
+                    } else {
+                        res.end(JSON.stringify(result_failed))
                     }
-                })
+                } else {
+                    res.end(JSON.stringify(result_failed))
+                }
             }
         })
     } else {
         res.end(JSON.stringify(result_failed))
     }
+}
+
+
+function usedleavedays(id, type) {
+    return new Promise(async function (resolve, reject) {
+        let sql     = `SELECT leaveDaysID FROM users WHERE UID = ${id}`
+        db.query(sql, async function (error, result) {
+            if (error) reject(error)
+            else {
+                let leaveDaysID     = result[0].leaveDaysID
+                let ID              = { id: Number(leaveDaysID) }
+                let mongodb         = await mongo()
+
+                mongodb.collection('leavedays').find(ID).toArray((error, result) => {
+                    if (error) reject(error)
+                    else {
+                        let value           = 0
+                        let rawResult       = result[0]
+                        let sick            = rawResult.sick
+                        let vacation        = rawResult.vacation
+                        let business        = rawResult.business
+                        let substitution    = rawResult.substitution
+                        switch (type) {
+                            case 'sick': {
+                                value = sick + 1
+                            } break
+                            case 'vacation': {
+                                value = vacation + 1
+                            } break
+                            case 'business': {
+                                value = business + 1
+                            } break
+                            case 'substitution': {
+                                value = substitution + 1
+                            } break
+                            default:
+                                break
+                        }
+                        let data            = { id, [type]: value }
+                        data                = { $set: data }
+                        
+                        mongodb.collection('leavedays').updateOne(ID, data, function (error, result) {
+                            if (error) reject(error)
+                            else resolve(result_success)
+                        })
+                    }
+                })
+            }
+        })
+    })
+}
+
+
+function updateleaves(insertId) {
+    return new Promise(function (resolve, reject) {
+        let sql     = `UPDATE   leaves
+                       SET      uploadID    = ${insertId}
+                       WHERE    leaveID     = ${insertId}`
+        db.query(sql, function (error, result) {
+            if (error) reject(error)
+            else resolve(result_success)
+        })
+    })
 }
 
 
