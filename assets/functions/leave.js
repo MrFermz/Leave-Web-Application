01a5b9aa633063905498b304b4f50e6bd7886d5e
MrFermz <Mr.Fermz@hotmail.com>
2020-02-17 17:14:19 +0700

    check leave MAX

--- assets/functions/leave.js
+++ assets/functions/leave.js
@@ -14,10 +14,35 @@ function onLoad() {
 async function genContent() {
     let sidebar         = await templateSidebar()
     let header          = await templateHeader()
+    let max             = await sqlQueriesGET('listsleavemax')
+    let remain          = await sqlQueriesGET('listsleavedays')
     let selector        = await templateLeaveSelector()
+    max                 = max[0]
+    remain              = remain[0]
     let cards           = await templateCardLeave()
     let markup          = sidebar + header + selector + cards
     document.getElementById('container').innerHTML  =   markup
+    if (remain.sick >= max.sick) {
+        document.getElementById('leave-select-sick').disabled                       = true
+        document.getElementById('leave-select-sick').checked                        = false
+        document.getElementById('leave-card-container').removeChild(document.getElementById('leave-card-sick'))
+        document.getElementById('leave-label-sick').style.textDecoration            = 'line-through'
+    }
+    if (remain.business >= max.business) {
+        document.getElementById('leave-select-business').disabled                   = true
+        document.getElementById('leave-card-container').removeChild(document.getElementById('leave-card-business'))
+        document.getElementById('leave-label-business').style.textDecoration        = 'line-through'
+    }
+    if (remain.vacation >= max.vacation) {
+        document.getElementById('leave-select-vacation').disabled                   = true
+        document.getElementById('leave-card-container').removeChild(document.getElementById('leave-card-vacation'))
+        document.getElementById('leave-label-vacation').style.textDecoration        = 'line-through'
+    } 
+    if (remain.substitution >= remain.substitution_max) {
+        document.getElementById('leave-select-substitution').disabled               = true
+        document.getElementById('leave-card-container').removeChild(document.getElementById('leave-card-substitution'))
+        document.getElementById('leave-label-substitution').style.textDecoration    = 'line-through'
+    }
 }
 
 
@@ -36,10 +61,15 @@ function onChangeLeaveType() {
             let dateEnd                     = document.getElementsByName(`date-end-${value}`)
             let file                        = document.getElementById(`upload-${value}`)
             let reasons                     = document.getElementsByName(`reasons-${value}`)
-            sick.style.display              = 'none'
-            business.style.display          = 'none'
-            vacation.style.display          = 'none'
-            substitution.style.display      = 'none'
+            if (sick) {
+                sick.style.display              = 'none'
+            } if (business) {
+                business.style.display          = 'none'
+            } if (vacation) {
+                vacation.style.display          = 'none'
+            } if (substitution) {
+                substitution.style.display      = 'none'
+            } 
             dateStart[0].value              = ''
             dateEnd[0].value                = ''
             if (file) {
@@ -91,7 +121,6 @@ function onChange() {
         }
         VALUES['reasons']   = REASONS[0].value
     }
-    console.log(VALUES)
 }
 
 
@@ -102,11 +131,13 @@ async function onSubmit() {
         file            = FILES[0]
     }
     let query           = await sqlQueriesLEAVE('createleaves', data)
-    if (query.result == 'success' && file) {
+    if (query.result == 'success' && file && (LEAVETYPE == 'sick' || LEAVETYPE == 'business')) {
         let data            = { id: query.data, file }
         let queryFile       = await queryUploader('uploaders', data)
         if (queryFile == 'success') {
-            console.log(queryFile)
+            location.reload()
         }
+    } else {
+        location.reload()
     }
 }
