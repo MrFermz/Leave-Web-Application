6c32187816d4c13a137909e2b1de5f6f886841ac
MrFermz <Mr.Fermz@hotmail.com>
2020-02-18 20:55:31 +0700

    change column name

--- api/method/get/lists_appr_users.js
+++ api/method/get/lists_appr_users.js
@@ -38,7 +38,7 @@ function getUsers(id) {
                                 firstname,
                                 lastname,
                                 nickname,
-                                departmentID,
+                                deptID,
                                 typeID,
                                 approverID,
                                 leavedaysID
