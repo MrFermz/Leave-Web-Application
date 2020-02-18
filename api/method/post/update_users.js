126ff7122838e49752906d7e1fabe7d5b34c2e5d
MrFermz <Mr.Fermz@hotmail.com>
2020-02-18 21:03:26 +0700

    add updateSubsMax and createApprover func

--- api/method/post/update_users.js
+++ api/method/post/update_users.js
@@ -1,5 +1,6 @@
 const db                                    = require('../../db_connection')
-const mongo                                 = require('../../mg_connection')
+const updateSubsMax                         = require('./update_subs_max')
+const createApprover                        = require('./create_approver')
 const { verifyToken }                       = require('../../jwt')
 const { result_success, result_failed }     = require('../result')
 
@@ -15,7 +16,7 @@ async function updateusers(req, res, body) {
                                   lastname      = '${data.lastname}', 
                                   nickname      = '${data.nickname}', 
                                   typeID        = '${data.usertype}', 
-                                  departmentID  = '${data.deptType}', 
+                                  deptID        = '${data.deptType}', 
                                   approverID    = '${data.approver}'
                            WHERE  UID           = ${data.UID}`
         db.query(sql, async function (error, result) {
@@ -23,10 +24,13 @@ async function updateusers(req, res, body) {
                 result_failed['data']   = error
                 res.end(JSON.stringify(result_failed))
             } else {
-                let subs        = await updateSubsMax(data.leaveDaysID, Number(data.subsMax))
-                if (subs.result == 'success') {
-                    res.end(JSON.stringify(result_success))
+                await updateSubsMax(data.leavedaysID, Number(data.subsMax))
+                if (data.makeAppr) {
+                    await createApprover(data.UID)
+                } else {
+                    await unmakeApprover(data.UID)
                 }
+                res.end(JSON.stringify(result_success))
             }
         })
     } else {
@@ -35,13 +39,10 @@ async function updateusers(req, res, body) {
 }
 
 
-function updateSubsMax(ID, subs) {
-    return new Promise(async function (resolve, reject) {
-        let mongodb         = await mongo()
-        let id              = { id: ID }
-        let substitution    = { substitution_max: subs }
-        let data            = { $set: substitution }
-        mongodb.collection('leavedays').updateOne(id, data, function (error, result) {
+function unmakeApprover(UID) {
+    return new Promise(function (resolve, reject) {
+        let sql = `DELETE FROM approver WHERE UID = ${UID}`
+        db.query(sql, function (error, result) {
             if (error) reject(error)
             else resolve(result_success)
         })
