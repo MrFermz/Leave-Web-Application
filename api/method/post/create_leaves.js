b0cd7274eb5e77bc89b349d7bb423eef4169f5d5
MrFermz <Mr.Fermz@hotmail.com>
2020-02-18 21:02:14 +0700

    change to SQL remove updateleaves func

--- api/method/post/create_leaves.js
+++ api/method/post/create_leaves.js
@@ -1,5 +1,4 @@
 const db                                    = require('../../db_connection')
-const mongo                                 = require('../../mg_connection')
 const { verifyToken }                       = require('../../jwt')
 const { result_success, result_failed }     = require('../result')
 
@@ -8,12 +7,14 @@ async function createleaves(req, res, body) {
 
     if (token) {
         let data        = JSON.parse(body)
+        let uploadID    = req.headers['uploadid']
         let sql         = `INSERT INTO leaves (leaveType, 
                                                dateStart, 
                                                dateEnd, 
                                                reasons, 
                                                status, 
-                                               UID) 
+                                               UID,
+                                               uploadID) 
                            VALUES ?`
         let values      = [[
                             data.leaveType,
@@ -21,26 +22,16 @@ async function createleaves(req, res, body) {
                             data.dateEnd,
                             data.reasons,
                             data.status,
-                            token.id
+                            token.id,
+                            uploadID
                           ]]
         db.query(sql, [values], async function (error, result) {
-            let insertId        = result.insertId
-            
             if (error) {
-                result_failed['data']   =   error
+                result_failed['data']   = error
                 res.end(JSON.stringify(result_failed))
             } else {
-                let leavedays       = await usedleavedays(token.id, data.leaveType)
-                if (leavedays.result == 'success') {
-                    let updateleave         = await updateleaves(insertId)
-                    if (updateleave.result == 'success') {
-                        res.end(JSON.stringify(result_success))
-                    } else {
-                        res.end(JSON.stringify(result_failed))
-                    }
-                } else {
-                    res.end(JSON.stringify(result_failed))
-                }
+                await usedleavedays(token.id, data.leaveType)
+                res.end(JSON.stringify(result_success))
             }
         })
     } else {
@@ -51,15 +42,13 @@ async function createleaves(req, res, body) {
 
 function usedleavedays(id, type) {
     return new Promise(async function (resolve, reject) {
-        let sql     = `SELECT leaveDaysID FROM users WHERE UID = ${id}`
+        let sql     = `SELECT leavedaysID FROM users WHERE UID = ${id}`
         db.query(sql, async function (error, result) {
             if (error) reject(error)
             else {
-                let leaveDaysID     = result[0].leaveDaysID
-                let ID              = { id: Number(leaveDaysID) }
-                let mongodb         = await mongo()
-
-                mongodb.collection('leavedays').find(ID).toArray((error, result) => {
+                let ID      = result[0].leavedaysID
+                let sql     = `SELECT * FROM leavedays WHERE leavedaysID = ${ID}`
+                db.query(sql, function (error, result) {
                     if (error) reject(error)
                     else {
                         let value           = 0
@@ -68,45 +57,31 @@ function usedleavedays(id, type) {
                         let vacation        = rawResult.vacation
                         let business        = rawResult.business
                         let substitution    = rawResult.substitution
+
                         switch (type) {
                             case 'sick': {
-                                value = sick + 1
+                                value   = sick + 1
                             } break
                             case 'vacation': {
-                                value = vacation + 1
+                                value   = vacation + 1
                             } break
                             case 'business': {
-                                value = business + 1
+                                value   = business + 1
                             } break
-                            case 'substitution': {
-                                value = substitution + 1
+                            case 'business': {
+                                value   = substitution + 1
                             } break
                             default:
                                 break
                         }
-                        let data            = { id, [type]: value }
-                        data                = { $set: data }
-                        
-                        mongodb.collection('leavedays').updateOne(ID, data, function (error, result) {
+                        let sql     = `UPDATE leavedays SET ${type} = ${value} WHERE leavedaysID = ${ID}`
+                        db.query(sql, function (error, result) {
                             if (error) reject(error)
                             else resolve(result_success)
                         })
                     }
                 })
             }
-        })
-    })
-}
-
-
-function updateleaves(insertId) {
-    return new Promise(function (resolve, reject) {
-        let sql     = `UPDATE   leaves
-                       SET      uploadID    = ${insertId}
-                       WHERE    leaveID     = ${insertId}`
-        db.query(sql, function (error, result) {
-            if (error) reject(error)
-            else resolve(result_success)
         })
     })
 }
