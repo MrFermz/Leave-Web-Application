525a46d36106eae85282667050458202a869c28f
MrFermz <Mr.Fermz@hotmail.com>
2020-02-18 21:03:52 +0700

    remove mongo new getLeaveDays func

--- api/method/get/lists_leaves_days.js
+++ api/method/get/lists_leaves_days.js
@@ -1,5 +1,4 @@
 const db                                    = require('../../db_connection')
-const mongo                                 = require('../../mg_connection')
 const { verifyToken }                       = require('../../jwt')
 const { result_success, result_failed }     = require('../result')
 
@@ -8,28 +7,19 @@ async function listsleavedays(req, res) {
     let token   = await verifyToken(req, res)
 
     if (token) {
-        let mongodb     = await mongo()
         let sql         = `SELECT leaveDaysID 
                            FROM   users
                            WHERE  UID = ${token.id}`
-        db.query(sql, function (error, result) {
+        db.query(sql, async function (error, result) {
             if (error) {
                 result_failed['data']   = error
                 res.end(JSON.stringify(result_failed))
             } else {
                 if (result.length > 0) {
-                    let leaveDaysID     = result[0].leaveDaysID
-                    let queryLeave      = { id: Number(leaveDaysID) }
-                    
-                    mongodb.collection('leavedays').find(queryLeave).toArray((error, result) => {
-                        if (error) {
-                            result_failed['data']   = error
-                            res.end(JSON.stringify(result_failed))
-                        } else {
-                            result_success['data']  = result
-                            res.end(JSON.stringify(result_success))
-                        }
-                    })
+                    let leaveDaysID         = result[0].leaveDaysID
+                    let days                = await getLeaveDays(leaveDaysID)
+                    result_success['data']  = days
+                    res.end(JSON.stringify(result_success))
                 } else {
                     result_failed['data']   = error
                     res.end(JSON.stringify(result_failed))
@@ -39,6 +29,17 @@ async function listsleavedays(req, res) {
     } else {
         res.end(JSON.stringify(result_failed))
     }
+}
+
+
+function getLeaveDays(id) {
+    return new Promise(function (resolve, reject) {
+        let sql     = `SELECT * FROM leavedays WHERE leavedaysID = ${id}`
+        db.query(sql, function (error, result) {
+            if (error) reject(error)
+            else resolve(result[0])
+        })
+    })
 }
 
 
