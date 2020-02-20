1c6d07c8f7b2d0f375e48de1c049f45829209f23
MrFermz <Mr.Fermz@hotmail.com>
2020-02-20 14:30:28 +0700

    change radio to button

--- assets/functions/leave.js
+++ assets/functions/leave.js
@@ -22,81 +22,88 @@ async function genContent() {
     let markup          = sidebar + header + selector + cards
     document.getElementById('container').innerHTML  =   markup
     if (remain.sick >= max.sick) {
-        document.getElementById('leave-select-sick').disabled                       = true
-        document.getElementById('leave-select-sick').checked                        = false
         document.getElementById('leave-card-container').removeChild(document.getElementById('leave-card-sick'))
-        document.getElementById('leave-label-sick').style.textDecoration            = 'line-through'
+        document.getElementById('leave-select-sick').disabled                       = true
+        document.getElementById('leave-select-sick').style.textDecoration           = 'line-through'
     }
     if (remain.business >= max.business) {
-        document.getElementById('leave-select-business').disabled                   = true
         document.getElementById('leave-card-container').removeChild(document.getElementById('leave-card-business'))
-        document.getElementById('leave-label-business').style.textDecoration        = 'line-through'
+        document.getElementById('leave-select-business').disabled                   = true
+        document.getElementById('leave-select-business').style.textDecoration       = 'line-through'
     }
     if (remain.vacation >= max.vacation) {
-        document.getElementById('leave-select-vacation').disabled                   = true
         document.getElementById('leave-card-container').removeChild(document.getElementById('leave-card-vacation'))
-        document.getElementById('leave-label-vacation').style.textDecoration        = 'line-through'
+        document.getElementById('leave-select-vacation').disabled                   = true
+        document.getElementById('leave-select-vacation').style.textDecoration       = 'line-through'
     } 
     if (remain.substitution >= remain.substitutionMax) {
-        document.getElementById('leave-select-substitution').disabled               = true
         document.getElementById('leave-card-container').removeChild(document.getElementById('leave-card-substitution'))
-        document.getElementById('leave-label-substitution').style.textDecoration    = 'line-through'
+        document.getElementById('leave-select-substitution').disabled               = true
+        document.getElementById('leave-select-substitution').style.textDecoration   = 'line-through'
     }
 }
 
 
-function onChangeLeaveType() {
-    let type        = document.getElementsByName('leaveType')
-    for (let i = 0; i < type.length; i++) {
-        if (type[i].checked) {
-            let value                       = type[i].value
-            LEAVETYPE                       = value
-            VALUES                          = undefined
-            let sick                        = document.getElementById('leave-card-sick')
-            let business                    = document.getElementById('leave-card-business')
-            let vacation                    = document.getElementById('leave-card-vacation')
-            let substitution                = document.getElementById('leave-card-substitution')
-            let dateStart                   = document.getElementsByName(`date-start-${value}`)
-            let dateEnd                     = document.getElementsByName(`date-end-${value}`)
-            let file                        = document.getElementById(`upload-${value}`)
-            let reasons                     = document.getElementsByName(`reasons-${value}`)
-            if (sick) {
-                sick.style.display              = 'none'
-            } if (business) {
-                business.style.display          = 'none'
-            } if (vacation) {
-                vacation.style.display          = 'none'
-            } if (substitution) {
-                substitution.style.display      = 'none'
-            } 
-            dateStart[0].value              = ''
-            dateEnd[0].value                = ''
-            if (file) {
-                file.value                      = ''
-            }
-            switch (value) {
-                case 'sick'             :   {
-                                                sick.style.display          = 'block'
-                                                reasons[0].value            = ''
-                                            }    
-                    break
-                case 'business'         :   {
-                                                business.style.display      = 'block'
-                                                reasons[0].value            = ''
-                                            } 
-                    break
-                case 'vacation'         :   {
-                                                vacation.style.display      = 'block'
-                                            } 
-                    break
-                case 'substitution'     :   {
-                                                substitution.style.display  = 'block'
-                                            } 
-                    break
-                default:            
-                    break
-            }
-        }
+function onChangeLeaveType(type) {
+    console.log(type)
+    LEAVETYPE                       = type
+    VALUES                          = undefined
+    let sick                        = document.getElementById('leave-card-sick')
+    let business                    = document.getElementById('leave-card-business')
+    let vacation                    = document.getElementById('leave-card-vacation')
+    let substitution                = document.getElementById('leave-card-substitution')
+    let sickSelect                  = document.getElementById('leave-select-sick')
+    let businessSelect              = document.getElementById('leave-select-business')
+    let vacationSelect              = document.getElementById('leave-select-vacation')
+    let substitutionSelect          = document.getElementById('leave-select-substitution')
+    let dateStart                   = document.getElementsByName(`date-start-${type}`)
+    let dateEnd                     = document.getElementsByName(`date-end-${type}`)
+    let file                        = document.getElementById(`upload-${type}`)
+    let reasons                     = document.getElementsByName(`reasons-${type}`)
+    if (sick) {
+        sickSelect.style.backgroundColor    = ''
+        sick.style.display              = 'none'
+    } if (business) {
+        businessSelect.style.backgroundColor    = ''
+        business.style.display          = 'none'
+    } if (vacation) {
+        vacationSelect.style.backgroundColor    = ''
+        vacation.style.display          = 'none'
+    } if (substitution) {
+        substitutionSelect.style.backgroundColor    = ''
+        substitution.style.display      = 'none'
+    } 
+    dateStart[0].value              = ''
+    dateEnd[0].value                = ''
+    if (file) {
+        file.value                      = ''
+    }
+    let bg_color        = 'gray'
+    switch (type) {
+        case 'sick'             :   {
+                                        sickSelect.style.backgroundColor        = bg_color
+                                        sick.style.display                      = 'block'
+                                        reasons[0].value                        = ''
+                                    }    
+            break
+        case 'business'         :   {
+                                        businessSelect.style.backgroundColor    = bg_color
+                                        business.style.display                  = 'block'
+                                        reasons[0].value                        = ''
+                                    } 
+            break
+        case 'vacation'         :   {
+                                        vacationSelect.style.backgroundColor    = bg_color
+                                        vacation.style.display                  = 'block'
+                                    } 
+            break
+        case 'substitution'     :   {
+                                        substitutionSelect.style.backgroundColor    = bg_color
+                                        substitution.style.display                  = 'block'
+                                    } 
+            break
+        default:            
+            break
     }
 }
 
@@ -110,8 +117,8 @@ function onChange() {
                             leaveType   : leaveType,
                             dateStart   : DATESTART[0].value,
                             dateEnd     : DATEEND[0].value,
-                            file        : '',
                             reasons     : '',
+                            uploadid    : null,
                             status      : 0
                           }
     if (leaveType == 'sick' || leaveType == 'business') {
@@ -128,8 +135,10 @@ async function onSubmit() {
     let file
     if (FILES) {
         file            = FILES[0]
+        let upload      = await queryUploader('uploaders', file)
+        let uploadID        = upload.data
+        data['uploadid']    = uploadID
     }
-    let upload          = await queryUploader('uploaders', file)
-    let uploadID        = upload.data
-    await sqlQueriesLEAVE('createleaves', data, uploadID)
+    console.log(data)
+    await sqlQueriesLEAVE('createleaves', data)
 }
