34184fd303be3c97dbb9b275febb697b75d3ede3
MrFermz <Mr.Fermz@hotmail.com>
2020-02-20 14:27:42 +0700

    add class div

--- assets/templates/template_leave.js
+++ assets/templates/template_leave.js
@@ -3,11 +3,9 @@ let LEAVE_TYPE      = ['sick', 'business', 'vacation', 'substitution']
 
 function templateLeaveSelector() {
     let markup      = `
-        <div id="leave-type-bar">
+        <div class="leave-type-bar" id="leave-type-bar">
             ${LEAVE_TYPE.map((ele, i) => { return (
-                `<input id="leave-select-${ele}" class="leave-select" type="radio" name="leaveType" onchange="onChangeLeaveType()"
-                    value="${ele}" ${ ele == 'sick' ? `checked="checked"` : `` }>
-                <label id="leave-label-${ele}">${ele}</label>`
+                `<input id="leave-select-${ele}" class="leave-select-${ele}" type="button" onclick="onChangeLeaveType('${ele}')" value="${ele.toUpperCase()}">`
             )}).join("")}
         </div>
     `
@@ -20,14 +18,19 @@ function templateCardLeave() {
         <div id="leave-card-container" class="leave-card">
             ${LEAVE_TYPE.map((ele, i) => { return (
                 `<div id="leave-card-${ele}" class="card center ${ele}">
-                    <h3 id="label-${ele}">${ele}</h3>
-                    Start: <input type="date" name="date-start-${ele}" onchange="onChange()"><br>
-                    End: <input type="date" name="date-end-${ele}" onchange="onChange()">
-                    ${ele == 'sick' || ele == 'business' ? `
-                        <input type="file" id="upload-${ele}" onchange="FILES = document.getElementById('upload-${ele}').files">
-                        Reasons: <input type="text" name="reasons-${ele}" placeholder="Reasons" onchange="onChange()">
-                    ` : ``}
-                    <input type="button" value="Submit" onclick="onSubmit()">
+                    <h3>${ele}</h3>
+                    <div class="leave-content">
+                        <div class="leave-label">START</div>
+                        <input type="date" name="date-start-${ele}" onchange="onChange()"><br>
+                        <div class="leave-label">END</div>
+                        <input type="date" name="date-end-${ele}" onchange="onChange()">
+                        ${ele == 'sick' || ele == 'business' ? `
+                            <input type="file" id="upload-${ele}" onchange="FILES = document.getElementById('upload-${ele}').files">
+                            Reasons: <input type="text" name="reasons-${ele}" placeholder="Reasons" onchange="onChange()">
+                        ` : ``}
+                        <div>SUMMARY</div>
+                        <input type="button" value="Submit" onclick="onSubmit()">
+                    </div>
                 </div>`
             )}).join("")}
         </div>
