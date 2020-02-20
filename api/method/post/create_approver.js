09b1154748b0c78b6bf050def3712daf8f29d460
MrFermz <Mr.Fermz@hotmail.com>
2020-02-20 14:32:44 +0700

    check duplicates approver

--- api/method/post/create_approver.js
+++ api/method/post/create_approver.js
@@ -4,13 +4,28 @@ const { result_success, result_failed }     = require('../result')
 
 function createApprover(UID) {
     return new Promise(function (resolve, reject) {
-        let sql = `INSERT INTO approver (UID) VALUES (${UID})`
+        let sql = `SELECT * FROM approver`
         db.query(sql, function (error, result) {
             if (error) {
-                result_failed['data']   = error
-                reject(result_failed)
-            } 
-            else resolve(result_success)
+                console.log(error)
+            } else {
+                let UIDlists    = []
+                result.forEach(ele => {
+                    UIDlists.push(ele.UID)
+                })
+                if (!UIDlists.includes(UID)) {
+                    let sql = `INSERT INTO approver (UID) VALUES (${UID})`
+                    db.query(sql, function (error, result) {
+                        if (error) {
+                            result_failed['data']   = error
+                            reject(result_failed)
+                        } 
+                        else resolve(result_success)
+                    })
+                } else {
+                    resolve(result_success)
+                }
+            }
         })
     })
 }
