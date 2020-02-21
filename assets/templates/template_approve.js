0d516c1d3738d205b8a9bf7907662e562201ca67
MrFermz <Mr.Fermz@hotmail.com>
2020-02-21 15:13:36 +0700

    adjust layout

--- assets/templates/template_approve.js
+++ assets/templates/template_approve.js
@@ -8,38 +8,82 @@ function templateCardApprove(list) {
         markup  = `<div>No more leave.</div>`
     }
     for (let i = 0; i < length; i++) {
-        const ele   = list[i]
-        let fragment = ``
+        const ele       = list[i]
+        let fragment    = ``
+        let dateStart   = formatDate(ele.dateStart)
+        let dateEnd     = formatDate(ele.dateEnd)
         if (ele.leaveType == 'sick' || ele.leaveType == 'business') {
             fragment = `
                 <div id="card-${i}" class="card center">
-                    <div id="nick-name-${i}">Nickname: ${ele.nickname}</div>
-                    <div id="employee-id-${i}">ID: ${ele.empID}</div>
-                    <div id="leave-type-${i}">Leave Type: ${ele.leaveType}</div>
-                    <div id="date-start-${i}">Date Start: ${ele.dateStart}</div>
-                    <div id="date-end-${i}">Date End: ${ele.dateEnd}</div>
-                    <div id="reasons-${i}">Reasons: ${ele.reasons}</div>
+                    <div class="parent">
+                        <div class="div1 avatar">
+                            <img src="../assets/images/account.svg">
+                        </div>
+                        <div class="div2">
+                            <div class="container-detail">
+                                <div class="detail" id="nick-name-${i}">Nickname: ${ele.nickname}</div>
+                                <div class="detail" id="employee-id-${i}">ID: ${ele.empID}</div>
+                                <div class="detail" id="leave-type-${i}">Leave: ${ele.leaveType}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="parent-date">
+                        <div class="div-1-date label">START</div>
+                        <div class="div-2-date value">${dateStart}</div>
+                        <div class="div-3-date label">END</div>
+                        <div class="div-4-date value">${dateEnd}</div>
+                    </div>
+                    <div class="days" id="days-${ele}">${rangeDays(new Date(ele.dateStart), new Date(ele.dateEnd))} DAYS</div>
+                    <div class="reasons" id="reasons-${i}">${ele.reasons}</div>
                     <img ${ele.img ? `src="../api/uploads/${ele.img}"` : ''} width="40%">
-                    <div id="input-container">
-                        <input type="button" value="Approve" onclick="onApprove(${ele.leaveID})">
-                        <input type="button" value="Reject">
+                    <div class="input-container" id="input-container">
+                        <input class="approve" type="button" value="Approve" onclick="onApprove(${ele.leaveID})">
+                        <input class="reject" type="button" value="Reject">
                     </div>
                 </div>
             `
         } else {
             fragment = `
                 <div id="card-${i}" class="card center">
-                    <div id="nick-name-${i}">Nickname: ${ele.nickname}</div>
-                    <div id="employee-id-${i}">ID: ${ele.empID}</div>
-                    <div id="leave-type-${i}">Leave Type: ${ele.leaveType}</div>
-                    <div id="date-start-${i}">Date Start: ${ele.dateStart}</div>
-                    <div id="date-end-${i}">Date End: ${ele.dateEnd}</div>
-                    <input type="button" value="Approve" onclick="onApprove(${ele.leaveID})">
-                    <input type="button" value="Reject">
+                    <div class="parent">
+                        <div class="div1 avatar">
+                            <img src="../assets/images/account.svg">
+                        </div>
+                        <div class="div2">
+                            <div class="container-detail">
+                                <div class="detail" id="nick-name-${i}">Nickname: ${ele.nickname}</div>
+                                <div class="detail" id="employee-id-${i}">ID: ${ele.empID}</div>
+                                <div class="detail" id="leave-type-${i}">Leave: ${ele.leaveType}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="parent-date">
+                        <div class="div-1-date label">START</div>
+                        <div class="div-2-date value">${dateStart}</div>
+                        <div class="div-3-date label">END</div>
+                        <div class="div-4-date value">${dateEnd}</div>
+                    </div>
+                    <div class="days" id="days-${ele}">${rangeDays(new Date(ele.dateStart), new Date(ele.dateEnd))} DAYS</div>
+                    <div class="input-container" id="input-container">
+                        <input class="approve" type="button" value="Approve" onclick="onApprove(${ele.leaveID})">
+                        <input class="reject" type="button" value="Reject">
+                    </div>
                 </div>
             `
         }
         markup += fragment
     }
     return markup
+}
+
+
+function formatDate(date) {
+    const months            = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    let DATE                = new Date(date)
+    let DATE_day            = DATE.getDate()
+    let DATE_month          = DATE.getMonth()
+    let DATE_month_short    = months[DATE_month]
+    let DATE_year           = DATE.getUTCFullYear()
+    DATE_final              = `${DATE_day} ${DATE_month_short} ${DATE_year}`
+    return DATE_final
 }
