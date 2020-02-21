f52b803f2881295f9001eeeb6a0144648ab26b79
MrFermz <Mr.Fermz@hotmail.com>
2020-02-21 10:12:23 +0700

    add LOGIN func

--- assets/queries/sql_query.js
+++ assets/queries/sql_query.js
@@ -1,26 +1,10 @@
 var http            = new XMLHttpRequest()  
 var TOKEN           = getToken()
-const config        = { host: '192.168.42.134', port: '8081' }
+const config        = { host: 'localhost', port: '8081' }
 
 function sqlQueriesGET(path) {
     http.open('GET', `http://${config.host}:${config.port}/${path}`, true)
     http.setRequestHeader('x-access-token', TOKEN)
-    http.send()
-    return new Promise(function (resolve, reject) {
-        http.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200) {
-                let data    = JSON.parse(this.responseText)
-                resolve(data.data)
-            }
-        }
-    })
-}
-
-
-function sqlQueriesLEAVEDAYS(path, id) {
-    http.open('GET', `http://${config.host}:${config.port}/${path}`, true)
-    http.setRequestHeader('x-access-token', TOKEN)
-    http.setRequestHeader('leaveDaysID', id)
     http.send()
     return new Promise(function (resolve, reject) {
         http.onreadystatechange = function () {
@@ -42,6 +26,37 @@ function sqlQueriesPOST(path, data) {
             if (this.readyState === 4 && this.status === 200) {
                 let data    = JSON.parse(this.responseText)
                 resolve(data.result)
+            }
+        }
+    })
+}
+
+
+function sqlQueriesLOGIN(path, data) {
+    http.open('POST', `http://${config.host}:${config.port}/${path}`, true)
+    http.setRequestHeader('x-access-token', TOKEN)
+    http.send(JSON.stringify(data))
+    return new Promise(function (resolve, reject) {
+        http.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                let data    = JSON.parse(this.responseText)
+                resolve(data)
+            }
+        }
+    })
+}
+
+
+function sqlQueriesLEAVEDAYS(path, id) {
+    http.open('GET', `http://${config.host}:${config.port}/${path}`, true)
+    http.setRequestHeader('x-access-token', TOKEN)
+    http.setRequestHeader('leaveDaysID', id)
+    http.send()
+    return new Promise(function (resolve, reject) {
+        http.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                let data    = JSON.parse(this.responseText)
+                resolve(data.data)
             }
         }
     })
