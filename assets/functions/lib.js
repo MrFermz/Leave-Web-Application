d0876489b07d1d7e344041d45ccedcf21838a6b2
MrFermz <Mr.Fermz@hotmail.com>
2020-02-24 19:35:45 +0700

    add formatdate func

--- assets/functions/lib.js
+++ assets/functions/lib.js
@@ -41,3 +41,27 @@ function rangeDays(start, end) {
     let result          = Math.round(Math.abs((start - end) / oneDay))
     return result + 1
 }
+
+
+function formatDate(date) {
+    const days              = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    const months            = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    let DATE                = new Date(date)
+    let DATE_day_short      = days[DATE.getDay()]
+    let DATE_day            = DATE.getDate()
+    let DATE_month          = DATE.getMonth()
+    let DATE_month_short    = months[DATE_month]
+    let DATE_year           = DATE.getUTCFullYear()
+    DATE_final              = `${DATE_day_short} ${DATE_day} ${DATE_month_short} ${DATE_year}`
+    return DATE_final
+}
+
+
+function formatDate2(date) {
+    let DATE                = new Date(date)
+    let DATE_day            = DATE.getDate()
+    let DATE_month          = DATE.getMonth() + 1
+    let DATE_year           = DATE.getUTCFullYear()
+    DATE_final              = `${DATE_year}-${DATE_month}-${DATE_day}`
+    return DATE_final
+}
