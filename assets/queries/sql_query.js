482ff43980b299240ad7c3ad330443fa744f1209
MrFermz <Mr.Fermz@hotmail.com>
2020-02-24 19:37:29 +0700

    add filter query

--- assets/queries/sql_query.js
+++ assets/queries/sql_query.js
@@ -108,3 +108,18 @@ function sqlQueriesAPPROVER(path, uid) {
         }
     })
 }
+
+
+function sqlQueriesFILTER(path, data) {
+    http.open('POST', `http://${config.host}:${config.port}/${path}`, true)
+    http.setRequestHeader('x-access-token', TOKEN)
+    http.send(JSON.stringify(data))
+    return new Promise(function (resolve, reject) {
+        http.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                let data    = JSON.parse(this.responseText)
+                resolve(data)
+            }
+        }
+    })
+}
