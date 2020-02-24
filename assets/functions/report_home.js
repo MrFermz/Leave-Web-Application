463b0436ca3b20c8f57991b264f080ba4638ef67
MrFermz <Mr.Fermz@hotmail.com>
2020-02-24 19:38:06 +0700

    add filter

--- assets/functions/report_home.js
+++ assets/functions/report_home.js
@@ -1,3 +1,5 @@
+var USERS
+
 function onLoad() {
     if (TOKEN) {
         genContent()
@@ -8,26 +10,63 @@ function onLoad() {
 
 
 async function genContent() {
-    let data            = await sqlQueriesGET('countleavesfilter')
-    console.log(data)
+    let data            = await sqlQueriesGET('countleavesfilterdefault')
     let count           = await sortUsers(data)
     let sidebar         = await templateSidebar()
     let header          = await templateHeader()
-    let filter          = await templateFilterReport()
+    let users           = await sqlQueriesGET('listsusers')
+    USERS               = users
+    let filter          = await templateFilterReport(users)
     let card            = await templateCardReport(count)
-    let markup          = sidebar + header + filter + card
+    let markup          = sidebar + header + filter
     document.getElementById('container').innerHTML = markup
+    document.getElementById('card-report-home').innerHTML = card
+}
+
+
+async function onChange() {
+    let start           = document.getElementsByName('date-start')
+    let end             = document.getElementsByName('date-end')
+    let user            = USERS.find((item) => { return item.nickname == document.getElementById('user').value })
+    start               = start[0].value
+    end                 = end[0].value
+    let data            = {}
+    
+    if (start && end) {
+        data['start']   = formatDate2(start)
+        data['end']     = formatDate2(end)
+    }
+    if (user) {
+        data['user']    = user.UID
+    }
+    if (!start && !end && !user) {
+        let data            = await sqlQueriesGET('countleavesfilterdefault')
+        let count           = await sortUsers(data)
+        let card            = await templateCardReport(count)
+        document.getElementById('card-report-home').removeChild(document.getElementById('card-report-main'))
+        document.getElementById('card-report-home').innerHTML   = card
+    }
+    let scope               = await sqlQueriesFILTER('countleavesfilter', data)
+    if (scope.result == 'success') {
+        let count           = await sortUsers(scope.data)
+        let card            = await templateCardReportFilter(count, data)
+        document.getElementById('card-report-home').removeChild(document.getElementById('card-report-main'))
+        document.getElementById('card-report-home').innerHTML   = card
+    } else {
+    }
 }
 
 
 function sortUsers(lists) {
     let data                = []
     return new Promise(function (resolve, reject) {
+        let username            = ''
         let sick                = 0
         let vacation            = 0
         let business            = 0
         let substitution        = 0
         for (const ele of lists) {
+            username            = ele.username
             switch (ele.leaveType) {
                 case 'sick'             : sick          = ele.cnt
                     break
@@ -41,9 +80,28 @@ function sortUsers(lists) {
                     break
             }
         }
-        data.push({ sick, vacation, business, substitution })
+        data.push({ username, sick, vacation, business, substitution })
         resolve(data)
     })
 }
 
 
+async function toggleCards() {
+    let card    = await templateCardReportDetail()
+    document.getElementById('card-report-home').removeChild(document.getElementById('card-report-main'))
+    document.getElementById('card-report-home').innerHTML   = card
+}
+
+
+async function backToHome() {
+    let data            = await sqlQueriesGET('countleavesfilterdefault')
+    let count           = await sortUsers(data)
+    let card            = await templateCardReport(count)
+    document.getElementById('card-report-home').removeChild(document.getElementById('card-report-detail'))
+    document.getElementById('card-report-home').innerHTML   = card
+}
+
+
+function onDetail() {
+    toggleCards()
+}
