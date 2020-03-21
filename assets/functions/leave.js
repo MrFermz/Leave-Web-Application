f02d5d5a7bed90212c1a7961c69b4d2cbe518b43
MrFermz <Mr.Fermz@hotmail.com>
2020-03-21 22:07:49 +0700

    rename, day off check

--- assets/functions/leave.js
+++ assets/functions/leave.js
@@ -23,35 +23,35 @@ async function onLoad() {
 async function genContent() {
     let sidebar         = await templateSidebar()
     let header          = await templateHeader()
-    let max             = await sqlQueriesGET('listsleavemax')
-    let remain          = await sqlQueriesGET('listsleavedays')
+    let capacity        = await sqlQueriesGET('listsleavecapacity')
+    let count           = await sqlQueriesGET('listsleavecount')
     let selector        = await templateLeaveSelector()
-    max                 = max.data[0]
-    remain              = remain.data
+    capacity            = capacity.data[0]
+    count               = count.data
     let cards           = await templateCardLeave(contentImages)
     let markup          = sidebar + header + selector + cards
     document.getElementById('container').innerHTML  =   markup
     let textColor       = 'lightgray'
-    if (remain.sick >= max.sick) {
+    if (count.sick >= capacity.sick) {
         document.getElementById('leave-card-container').removeChild(document.getElementById('leave-card-sick'))
         document.getElementById('leave-select-sick').onclick                        = ''
         document.getElementById('leave-select-sick').style.color                    = textColor
         document.getElementById('leave-select-sick').style.backgroundColor          = '#F2F3F4'
         document.getElementById('leave-select-sick').onmouseover = function () { hover('sick') }
     }
-    if (remain.business >= max.business) {
+    if (count.business >= capacity.business) {
         document.getElementById('leave-card-container').removeChild(document.getElementById('leave-card-business'))
         document.getElementById('leave-select-business').onclick                    = ''
         document.getElementById('leave-select-business').style.color                = textColor
         document.getElementById('leave-select-business').onmouseover = function () { hover('business') }
     }
-    if (remain.vacation >= max.vacation) {
+    if (count.vacation >= capacity.vacation) {
         document.getElementById('leave-card-container').removeChild(document.getElementById('leave-card-vacation'))
         document.getElementById('leave-select-vacation').onclick                    = ''
         document.getElementById('leave-select-vacation').style.color                = textColor
         document.getElementById('leave-select-vacation').onmouseover = function () { hover('vacation') }
     } 
-    if (remain.substitution >= remain.substitutionMax) {
+    if (count.substitution >= count.substitutionMax) {
         document.getElementById('leave-card-container').removeChild(document.getElementById('leave-card-substitution'))
         document.getElementById('leave-select-substitution').onclick                = ''
         document.getElementById('leave-select-substitution').style.color            = textColor
@@ -103,6 +103,7 @@ function onChangeLeaveType(type) {
         file.value                  = null
         fileLabel.innerHTML         = '<label>No file </label><span>*</span>'
     }
+    document.getElementById('container-upload').style.display   = 'none'
     switch (type) {
         case 'sick'             :   {
                                         sickSelect.style.backgroundColor        = '#E74C3C'
@@ -113,7 +114,6 @@ function onChangeLeaveType(type) {
         case 'business'         :   {
                                         businessSelect.style.backgroundColor    = '#3498DB'
                                         business.style.display                  = 'block'
-                                        reasons[0].value                        = ''
                                     } 
             break
         case 'vacation'         :   {
@@ -143,12 +143,19 @@ async function onChange() {
     DATESTART           = document.getElementsByName(`date-start-${leaveType}`)
     DATEEND             = document.getElementsByName(`date-end-${leaveType}`)
     let summaryValue    = await summary(DATESTART[0].value, DATEEND[0].value)
+    let matchDaysStart  = await matchDay(DATESTART[0].value)
+    let matchDaysEnd    = await matchDay(DATEEND[0].value)
     document.getElementById(`summary-${leaveType}`).innerHTML    = summaryValue.sum
     if (summaryValue.days > -1) {
         document.getElementById(`days-${leaveType}`).innerHTML   = `${summaryValue.days} DAYS`
     }
     if (summaryValue.days == 0) {
         document.getElementById(`days-${leaveType}`).innerHTML   = `1 DAYS`
+    }
+    if (summaryValue.days > 1 || (matchDaysStart == 'Fri' || matchDaysStart == 'Mon') || (matchDaysEnd == 'Fri' || matchDaysEnd == 'Mon')) {
+        document.getElementById('container-upload').style.display   = 'block'
+    } else {
+        document.getElementById('container-upload').style.display   = 'none'
     }
     REASONS             = document.getElementsByName(`reasons-${leaveType}`)
     VALUES              = {
@@ -171,28 +178,37 @@ async function onChange() {
 async function onSubmit() {
     let message, name, ext, size, file
     let data            = VALUES
-    if (FILES && (data.leaveType == 'sick' || data.leaveType == 'business') && (data.dateStart !== '' && data.dateEnd !== '' && data.reasons !== '')) {
-        file                = FILES[0]
-        size                = file.size
-        size                = Number(((size / 1024) / 1024).toFixed(2))
-        name                = file.name.split('.')
-        ext                 = name[name.length - 1]
-        if (size <= 10 && contentImages.includes(ext)) {
-            let upload          = await sqlQueriesPOST('uploaders', file, 'file')
-            let uploadID        = upload.data
-            data['uploadid']    = uploadID
+    if ((data.leaveType == 'sick') && (data.dateStart !== '' && data.dateEnd !== '' && data.reasons !== '')) {
+        if (FILES) {
+            file                = FILES[0]
+            size                = file.size
+            size                = Number(((size / 1024) / 1024).toFixed(2))
+            name                = file.name.split('.')
+            ext                 = name[name.length - 1]
+            if (size <= 10 && contentImages.includes(ext)) {
+                let upload          = await sqlQueriesPOST('uploaders', file, 'file')
+                let uploadID        = upload.data
+                data['uploadid']    = uploadID
+                let result          = await sqlQueriesPOST('createleaves', data)
+                if (result.result == 'success') {
+                    location.reload()
+                }
+            } else {
+                let ext         = ''
+                contentImages.forEach(ele => { ext += ' ' + ele })
+                message         = `File must be less than 10MB\nFile ext is not${ext}.`
+                alert(message)
+            }
+        } else {
+            data['uploadid']    = 1
             let result          = await sqlQueriesPOST('createleaves', data)
             if (result.result == 'success') {
                 location.reload()
             }
-        } else {
-            let ext         = ''
-            contentImages.forEach(ele => { ext += ' ' + ele })
-            message         = `File must be less than 10MB\nFile ext is not${ext}.`
-            alert(message)
         }
     }
-    if ((data.leaveType == 'vacation' || data.leaveType == 'substitution') && (data.dateStart !== '' && data.dateEnd !== '')) {
+    if ((data.leaveType == 'vacation' || data.leaveType == 'substitution' || data.leaveType == 'business') && (data.dateStart !== '' && data.dateEnd !== '')) {
+        data['uploadid']    = 1
         let result          = await sqlQueriesPOST('createleaves', data)
         if (result.result == 'success') {
             location.reload()
@@ -219,7 +235,7 @@ function summary(dateStart, dateEnd) {
     // END
     let end                 = new Date(dateEnd)
     let end_day             = end.getDate()
-    let end_day_short     = days[end.getDay()]
+    let end_day_short       = days[end.getDay()]
     let end_month           = end.getMonth()
     let end_month_short     = months[end_month]
     let end_year            = end.getUTCFullYear()
