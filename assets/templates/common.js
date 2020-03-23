23b1f5a9cb0345403d7cb3213547a3a623c1d8a3
MrFermz <Mr.Fermz@hotmail.com>
2020-03-23 20:01:01 +0700

    change URL to URI, add info to side-menu

--- assets/templates/common.js
+++ assets/templates/common.js
@@ -1,32 +1,43 @@
 var TYPE            = Number(localStorage.getItem('type'))
 var NICKNAME        = localStorage.getItem('nickname')
-var URL             = URLsplit()
-var currentURL      = currentURL(URL)
+var URI             = URLsplit()
+var currentURL      = currentURL(URI)
 document.title      = currentURL
+var TYPE_NAME       = ['admin', 'employee', 'supervisor', 'hr', 'producer']
 function templateSidebar() {
     let markup = `
         <div id="side-container" class="side-container">
             <div id="side-bar" class="side-bar">
-                <div class="menu-item-close" onclick="closeSidebar()">
-                    <a class="item-close">| | |</a>
+                <div>
+                    <div class="menu-item-close" onclick="closeSidebar()">
+                        <a class="item-close">| | |</a>
+                    </div>
+                    <div>
+                        ${NICKNAME} [${TYPE_NAME[TYPE][0].toUpperCase() + TYPE_NAME[TYPE].slice(1)}]
+                    </div>
+                    <div>
+                        ${[0, 3, 4].includes(TYPE)
+                            ? `<div class="menu-item-header">Management</div>
+                                <input class="menu-item" type="button" value="User Manage" onclick="onUsersManage()" ${URI == 'user_manage' ? 'style="background-color: #85929E"' : ''}>
+                                ${TYPE !== 4 
+                                    ? `<input class="menu-item" type="button" value="Leave day manage" onclick="onLeaveManage()" ${URI == 'leave_manage' ? 'style="background-color: #85929E"' : ''}>`
+                                    : ''}`
+                            : ''}
+                    </div>
+                    <div>
+                        <div class="menu-item-header">History</div>
+                        <input class="menu-item" type="button" value="Leave history" onclick="onHistoryLeave()" ${URI == 'history_leave' ? 'style="background-color: #85929E"' : ''}>
+                        ${[0, 2, 3, 4].includes(TYPE)
+                            ? `<input class="menu-item" type="button" value="Approve history" onclick="onHistoryApprove()" ${URI == 'history_approve' ? 'style="background-color: #85929E"' : ''}>`
+                            : ''}
+                    </div>
                 </div>
-                ${[0, 3, 4].includes(TYPE)
-                        ? `<div class="menu-item-header">Management</div>
-                            <input class="menu-item" type="button" value="User Manage" onclick="onUsersManage()" ${URL == 'user_manage' ? 'style="background-color: #85929E"' : ''}>
-                            ${TYPE !== 4 
-                                ? `<input class="menu-item" type="button" value="Leave day manage" onclick="onLeaveManage()" ${URL == 'leave_manage' ? 'style="background-color: #85929E"' : ''}>`
-                                : ''}`
+                <div>
+                    ${[0].includes(TYPE)
+                        ? `<!--<input class="menu-item" type="button" value="Token" onclick="checkToken()">-->`
                         : ''}
-                <div class="menu-item-header">History</div>
-                <input class="menu-item" type="button" value="Leave history" onclick="onHistoryLeave()" ${URL == 'history_leave' ? 'style="background-color: #85929E"' : ''}>
-                ${[0, 2, 3, 4].includes(TYPE)
-                    ? `<input class="menu-item" type="button" value="Approve history" onclick="onHistoryApprove()" ${URL == 'history_approve' ? 'style="background-color: #85929E"' : ''}>`
-                    : ''}
-                <div class="menu-item-header">Others</div>
-                ${[0].includes(TYPE)
-                        ? `<input class="menu-item" type="button" value="Token" onclick="checkToken()">`
-                        : ''}
-                <input class="menu-item" type="button" value="Logout" onclick="onLogout()">
+                    <input class="menu-item logout" type="button" value="LOGOUT" onclick="onLogout()">
+                </div>
             </div>
         </div>
     `
@@ -37,15 +48,14 @@ function templateSidebar() {
 function templateHeader() {
     let markup = `
         <div id="header" class="header">
-            <!-- <a class="side-burger-container" onclick="openSidebar()"><i class="fas fa-bars fa-2x"></i></a> -->
             <a class="side-burger-container-open" onclick="openSidebar()">| | |</a>
-            <div class="header-name">${currentURL} (${NICKNAME})</div>
+            <div class="header-name">${currentURL}</div>
         </div>
         <div id="menu-top" class="menu-top">
-            <input id="header-menu-home" type="button" value="Home" onclick="onHome()" ${URL == 'home' ? `style="background-color: #85929E"` : ''}>
-            <input id="header-menu-leave" type="button" value="Leave" onclick="onLeave()" ${URL == 'leave' ? `style="background-color: #85929E"` : ''}>
-            ${ TYPE != 1 ? `<input id="header-menu-approve" type="button" value="Approve" onclick="onApprove()" ${URL == 'approve' ? `style="background-color: #85929E"` : ''}>` : '' }
-            ${[0, 3, 4].includes(TYPE) ? `<input id="header-menu-report" type="button" value="Report" onclick="onReport()" ${URL == 'report_home' || URL == 'report_detail' ? `style="background-color: #85929E"` : ''}>` : '' }
+            <input id="header-menu-home" type="button" value="Home" onclick="onHome()" ${URI == 'home' ? `style="background-color: #85929E"` : ''}>
+            <input id="header-menu-leave" type="button" value="Leave" onclick="onLeave()" ${URI == 'leave' ? `style="background-color: #85929E"` : ''}>
+            ${ TYPE != 1 ? `<input id="header-menu-approve" type="button" value="Approve" onclick="onApprove()" ${URI == 'approve' ? `style="background-color: #85929E"` : ''}>` : '' }
+            ${[0, 3, 4].includes(TYPE) ? `<input id="header-menu-report" type="button" value="Report" onclick="onReport()" ${URI == 'report_home' || URI == 'report_detail' ? `style="background-color: #85929E"` : ''}>` : '' }
         </div>
     `
     return markup
