0fad6947be5e82f3853cebbae8c10199368561f7
MrFermz <Mr.Fermz@hotmail.com>
2020-03-24 17:20:40 +0700

    add requesting card,

--- assets/functions/home.js
+++ assets/functions/home.js
@@ -16,10 +16,43 @@ async function genContent() {
     let leaveCount      = await sqlQueriesGET('listsleavecount')
     let leaveCapacity   = await sqlQueriesGET('listsleavecapacity')
     let pendings        = await sqlQueriesGET('listspendings')
+    let reqUsers        = await sqlQueriesGET('listsapprusers')
+    let reqLeaves       = await sqlQueriesGET('listsappleaves')
+    let users
+    if (reqUsers.data) {
+        users           = reqUsers.data.rawdata
+    }
+    let leaves          = reqLeaves.data
+    let requesting
+    if (users) {
+        let lists       = await sortedLists(users, leaves)
+        lists           = lists.length
+        requesting      = await templateCardHome(lists, 'requesting')
+    } else {
+        requesting      = ''
+    }
     let sidebar         = await templateSidebar()
     let header          = await templateHeader()
     let cardLeave       = await templateCardHome(leaveCount, 'leave', leaveCapacity)
     let cardPending     = await templateCardHome(pendings, 'pending')
-    let markup          = sidebar + header + cardLeave + cardPending
+    let markup          = sidebar + header + cardLeave + cardPending + requesting
     document.getElementById('container').innerHTML  =   markup
+}
+
+
+function sortedLists(users, leaves) {
+    let data                = []
+    let img
+    return new Promise(function (resolve, reject) {
+        for (const leave of leaves) {
+            for (const user of users) {
+                if (user.UID == leave.UID) {
+                    data.push({
+                        leaveID     : leave.leaveID
+                    })
+                }
+            }
+        }
+        resolve(data)
+    })
 }
