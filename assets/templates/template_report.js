6014e8bbe10fb0bcee6a73ab440ef574ba919c4d
MrFermz <Mr.Fermz@hotmail.com>
2020-03-24 17:29:22 +0700

    add default date

--- assets/templates/template_report.js
+++ assets/templates/template_report.js
@@ -1,4 +1,9 @@
 function templateFilterReport(users) {
+    let now         = new Date()
+    let past        = new Date(now.setDate(now.getDate() - 31))
+    let today       = new Date()
+    let dateStart   = formatDefaultDate(`${today.getFullYear()}`, `${today.getMonth() + 1}`, `${today.getDate()}`)
+    let dateEnd     = formatDefaultDate(`${past.getFullYear()}`, `${past.getMonth() + 1}`, `${past.getDate()}`)
     let url         = URLsplit()
     let usersArray  = ''
     let markup      = ''
@@ -6,9 +11,9 @@ function templateFilterReport(users) {
         markup      = `
             <div id="desktop-container" class="center">
                 <div class="desktop-label">START: </div>
-                <input name="date-start" type="date" onchange="onChange()">
+                <input name="date-start" type="date" onchange="onChange()" value="${dateEnd}">
                 <div class="desktop-label">END: </div>
-                <input name="date-end" type="date" onchange="onChange()">
+                <input name="date-end" type="date" onchange="onChange()" value="${dateStart}">
                 <div class="desktop-label">USERS: </div>
                 <input id="user" class="filter-users" list="users" onchange="onChange()" autocomplete="off">
                 ${users.map(user => { usersArray += `<option data=${user.UID} value="${user.nickname}">(${user.empID}) ${user.firstname} ${user.lastname}</option>` }).join("")}
@@ -20,9 +25,9 @@ function templateFilterReport(users) {
         markup      = `
             <div id="desktop-container" class="center">
                     <div class="desktop-label">START: </div>
-                    <input name="date-start" type="date" onchange="onChange()">
+                    <input name="date-start" type="date" onchange="onChange()" value="${dateEnd}">
                     <div class="desktop-label">END: </div>
-                    <input name="date-end" type="date" onchange="onChange()">
+                    <input name="date-end" type="date" onchange="onChange()" value="${dateStart}">
                     <div class="desktop-label">USERS: </div>
                     <input id="user" class="filter-users" list="users" onchange="onChange()" autocomplete="off">
                     ${users.map(user => { usersArray += `<option data=${user.UID} value="${user.nickname}">(${user.empID}) ${user.firstname} ${user.lastname}</option>` }).join("")}
