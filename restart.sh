3f05bf46414a12cf5806072c1eb7d0989d3a2d5c
root <root@localhost.localdomain>
2020-04-08 13:40:12 +0700

    update restart file

--- restart.sh
+++ restart.sh
@@ -3,4 +3,4 @@
 docker stop nginx
 docker rm nginx
 
-docker run --name nginx --network=leaveApp -v /home/docker/nginx:/etc/nginx/conf.d -v /home/docker/leaveApp:/usr/share/nginx/html -v /home/docker/uploads:/usr/share/nginx/html/api/uploads -p 80:80 --restart always -d nginx:1.17.8
+docker run --name nginx --network=leaveApp -v /home/nginx:/etc/nginx/conf.d -v /home/leaveApp:/usr/share/nginx/html -v /home/uploads:/usr/share/nginx/html/api/uploads -p 80:80 --restart always -d nginx:1.17.8
